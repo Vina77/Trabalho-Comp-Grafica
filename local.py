5b88eeff010856dd21a5fb71db7321ed370f698e
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-10-17 18:01:56 -0300

    main, biblioteca, funcao local atualizada

--- local.py
+++ local.py
@@ -1,4 +1,25 @@
+import os 
+
 def local():
+   
     #pasta local onde os arquivos imagens serão salvos.
     #retorna o endereço completo da imagem
-    pass
+    list=[]
+    path = (r"C:\Users")
+    dir_list = os.listdir(path) 
+    
+    print("Qual é o seu usuario? ", path, " :")  
+    list.append(dir_list)
+    for x in list:
+        cont=len(x)
+        for i in range(cont):
+            print(i,"-",x[i])
+    
+            
+    user=int(input("Digite o numero: "))
+    for x in list:
+       print(x[user])
+    
+      
+       
+local()
