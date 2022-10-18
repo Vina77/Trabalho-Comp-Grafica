b51846160cddaba3d65ce62ca93b9f6a37e642b7
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-10-18 14:56:02 -0300

    mudancas na main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from leitura import leitura
+#Prototipo de programa que le uma imagem texto e pega seus dados para criar uma planilha
 def main():
+    print("Favor salve sua imagem em Downloads")
+    
     leitura()
     
 main()
