01e0c6a0555e5e341ba9f77f81ea1a1e53c03264
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-10-20 16:20:22 -0300

    alteracao em tratamento de texto

--- leitura.py
+++ leitura.py
@@ -10,16 +10,11 @@ def leitura_de_imagem():
     altura, largura = imagem.shape[0:2] 
     if  altura>500 and largura>500:
         imagem=cv.resize(imagem,(0,0),fx=0.5,fy=0.5)        #verificação do tamanho da imagem para redimensionamento
-                                                
+                                             
+    
     imagem_cinza = cv.cvtColor(imagem, cv.COLOR_BGR2GRAY)  #metodo para transfiormar uma imagem colorida em preto e branco   
     
     imagem_board=cv.Laplacian(imagem_cinza,cv.CV_8U)       #metodo de detecção de bordas
-    
-    
-    cv.imshow("Live", imagem_board)
-
-    key = cv.waitKey(0) 
-    cv.destroyAllWindows()  
     
     
     
@@ -27,16 +22,17 @@ def leitura_de_imagem():
     ts.pytesseract.tesseract_cmd = (caminho + r'\tesseract.exe')    #tessaract serve para transformar a imagem em texto
     teste_texto=ts.image_to_string(imagem)
     with open("texto_temp.txt", "w") as arquivo:
-        arquivo.write(teste_texto)
+        arquivo.write(teste_texto)                  #teste para verificar se ele conseguiu ler alguma coisa
     retorno=os.path.getsize("texto_temp.txt")
     print(retorno)
     
     if retorno==0:
         os.remove("texto_temp.txt")
-        texto=ts.image_to_boxes(imagem_board)
+        texto=ts.image_to_boxes(imagem_board)           #caso for zero ele vai tentar verificar pelas bordas
         
     else:
-        texto=ts.image_to_boxes(imagem_cinza)
+        os.remove("texto_temp.txt")
+        texto=ts.image_to_boxes(imagem_cinza)           #se não uma imagem preto e branco
         
     
                                                                    
--- tratamento_de_texto.py
+++ tratamento_de_texto.py
@@ -1,6 +1,9 @@
 import os
 
 def trantamento_texto(texto):
+    numeros=['0','1','2','3','4','5','6','7','8','9']
+    add_numeros=[]
+    add_Letras=[]
     
     with open("valores_temp.txt", "w") as arquivo:
         arquivo.write(texto)
@@ -8,15 +11,16 @@ def trantamento_texto(texto):
     
     with open("valores_temp.txt", "r") as arquivo:
         arq=arquivo.read()
-    print(arq)
+    for array in arq.splitlines():
+        array=array.split(' ')
+        if array[0]=='0'or array[0]=='1'or array[0]=='2'or array[0]=='3'or array[0]=='4' or array[0]=='5' or array[0]=='6' or array[0]=='7' or array[0]=='8' or array[0]== '9':
+            add_numeros.append(array[0])
+        else:
+            add_Letras.append(array[0])
+                    
+    os.remove("valores_temp.txt")
+    print(add_Letras)
+  
     
-    
-    
-    """
-    def etc():
-    path = "diretorio"`
-    dir = os.listdir(path)              #excuir arquivo temporario após pegar seus dados
-    for file in dir:
-        if file == "arquivo.txt":
-            os.remove(file)"""
+  
             
