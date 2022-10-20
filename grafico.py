b9eadd64090d4b7a062f64d1ee6d03d1c8449b27
Marcus Nóbrega <69904672+Vina77@users.noreply.github.com>
2022-10-19 23:06:21 -0300

    Add files via upload

---
+++ grafico.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from local import local
+
+#Chama a classe "local" para achar o nome da planilha
+arquivo = local()
+
+#Le a planilha
+df_arquivo = pd.read_excel(arquivo)
+
+#Executa a planilha e exibe um grafica de bara
+df_arquivo.plot(kind='bar')
+plt.show()
+
