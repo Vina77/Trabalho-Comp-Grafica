05950991cf0722ace659b6a6c652aa3cc8697b1f
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-10-22 19:32:46 -0300

    separação de numeros e tratamento de numeros completo

--- leitura.py
+++ leitura.py
@@ -1,21 +1,24 @@
 import cv2 as cv
 import pytesseract as ts
 from local import local
-from tratamentos import tratamento_imagem, tratamento_texto
+from tratamentos import  tratamento_texto
 import os
 
 def leitura_de_imagem():
     caminho_imagem=local()
     imagem=cv.imread(caminho_imagem)    #leitura da imagem
     altura, largura = imagem.shape[0:2] 
-    if  altura>500 and largura>500:
+   
+    if  altura>800 and largura>800:
         imagem=cv.resize(imagem,(0,0),fx=0.5,fy=0.5)        #verificação do tamanho da imagem para redimensionamento
-                                             
+                                           
     
     imagem_cinza = cv.cvtColor(imagem, cv.COLOR_BGR2GRAY)  #metodo para transfiormar uma imagem colorida em preto e branco   
     
     imagem_board=cv.Laplacian(imagem_cinza,cv.CV_8U)       #metodo de detecção de bordas
     
+    cv.imshow("analise",imagem)
+    cv.waitKey(0)
     
     
     caminho=(r'C:\Users\Vivo\AppData\Local\Tesseract-OCR')
@@ -25,7 +28,7 @@ def leitura_de_imagem():
     with open("texto_temp.txt", "w") as arquivo:
         arquivo.write(teste_texto)                  #teste para verificar se ele conseguiu ler alguma coisa
     retorno=os.path.getsize("texto_temp.txt")
-    print(retorno)
+    
     
     if retorno==0:
         os.remove("texto_temp.txt")
@@ -35,7 +38,6 @@ def leitura_de_imagem():
         os.remove("texto_temp.txt")
         texto=ts.image_to_boxes(imagem_cinza)           #se não uma imagem preto e branco
        
-    
-    tratamento_imagem(teste_texto)                                                               
-    tratamento_texto(texto)
+                                                                   
+    tratamento_texto(texto,teste_texto)
     
--- tratamentos.py
+++ tratamentos.py
@@ -1,10 +1,12 @@
 import os
-import cv2 as cv
-import pytesseract as ts
 
-def tratamento_texto(texto):
+from numpy import append
+
+
+def tratamento_texto(texto,textolista):
     add_numeros=[]                      #lista para separação de letras e numeros 
-    add_Letras=[]
+    add_Letras=[" ","{"," ","","}","_","'"]
+    
     
     with open("valores_temp.txt", "w") as arquivo:
         arquivo.write(texto)                                #cria um arquivo tmp para tratar
@@ -20,17 +22,15 @@ def tratamento_texto(texto):
         else:
             add_Letras.append(array[0])
         
-        os.remove("valores_temp.txt")           #apaga o arquivo         
+    os.remove("valores_temp.txt")           #apaga o arquivo         
+    list_texto=[textolista]
+    tratado=[]
     
-def tratamento_imagem(texto):
-    pass
-    
-    
-    
-    
-    
-    
-    
-    
-
-            
+    for palavras in range(len(list_texto)):
+        tratando=list_texto[palavras]                       #remover impurezas que acompanham os numeros
+        for x in range(0,len(add_Letras)):
+            tratando=tratando.replace(add_Letras[x],"")
+            tratado.append(tratando)
+   
+    for x in add_Letras:
+        print(x)           
