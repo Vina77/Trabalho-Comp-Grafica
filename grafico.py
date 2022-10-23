142b82d1f8ba870eeac69d70ffc7c4de66399f7e
Marcus Nóbrega <69904672+Vina77@users.noreply.github.com>
2022-10-22 21:26:58 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -4,11 +4,37 @@ from local import local
 
 #Chama a classe "local" para achar o nome da planilha
 arquivo = local()
-
 #Le a planilha
 df_arquivo = pd.read_excel(arquivo)
 
-#Executa a planilha e exibe um grafica de barra
-df_arquivo.plot(kind='bar')
-plt.show()
+aux = int(input("Escolha 1 para grafico em barra ou 2 para o grafico em pizza: "))
 
+if aux == 1:
+    #Define o nome do eixo X
+    eixoX = input("Coloque o nome para o eixo X: ")
+    print("Nome do eixo X: " + eixoX)
+
+    #Define o nome do eixo Y
+    eixoY = input("Coloque o nome para o eixo Y: ")
+    print("Nome do eixo Y: " + eixoY)
+
+    #Exibe um grafico de barra
+    df_arquivo.plot(kind='bar')
+    plt.xlabel(eixoX)
+    plt.ylabel(eixoY)
+    plt.show()
+
+elif aux == 2:
+    #Define a coluna que sera usada para construir o grafico
+    dadoGraf = input("Coloque a coluna do dado para construir o gráfico: ")
+    print("A coluna é: " + dadoGraf)
+    
+    #Define a coluna com os nomes dos dados acima
+    labelName = input("Coloque a coluna com os respectivos nomes dos dados escolhidos: ")
+    print("A coluna é: " + labelName)
+
+    #Exibe um grafico de pizza
+    plt.pie(df_arquivo[dadoGraf], labels = df_arquivo[labelName])
+    plt.show()
+else:
+    print("Opção invalida")
